724eff03cbf4767ba87440f26dd04d20074ff6bf
dwightmayer <d.mayer@northeastern.edu>
2024-10-29 16:30:24 -0400

    gets size in bytes

--- training.py
+++ training.py
@@ -156,6 +156,7 @@ def main():
 
     batch = next(iter(dl))
     dataset_cc = Dataset.from_dict(batch)
+    print(dataset_cc.info['size_in_bytes'])
     print('Batched Dataset Loaded')
 
     print('Beginning tokenization')
